7e602ebffeaa3d658fae1915ac62bd2f124da14e
Olubunmi Bakre <bakreolubunmi@yahoo.com>
2022-04-03 10:42:45 +0100

    Bar chart assignment is now updated

--- src/chapterFourAndFive/BarChart.java
+++ src/chapterFourAndFive/BarChart.java
@@ -6,13 +6,17 @@ public class BarChart {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter number: ");
-        int a = input.nextInt();
-        while (a >= 1 && a<= 30) {
-            System.out.println("number");
-            a = input.nextInt();
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Enter number between 1 and 30: ");
+            int a = input.nextInt();
+
+            for (int j = 1; j <= a; j++) {
+                System.out.print("*");
+            }
+
         }
 
-
     }
+
 }
