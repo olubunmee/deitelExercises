292da38706120d95eeeeee3c220f5a6ca68d9383
Olubunmi Bakre <bakreolubunmi@yahoo.com>
2022-04-05 13:23:31 +0100

    arrays classwork

--- src/chapterEight/StudentScores.java
+++ src/chapterEight/StudentScores.java
@@ -8,19 +8,26 @@ public class StudentScores {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        int[][] grade = new int[3][3];
+
+        System.out.println("Enter number of students: ");
+        int students = input.nextInt();
+
+        System.out.println("Enter number of subjects: ");
+        int subjects = input.nextInt();
+
+        int[][] grade = new int[students][subjects];
 
         for (int i = 0; i <= 2; i++){
             System.out.println("Enter Student number: ");
             grade [i][i] = input.nextInt();
             for (int j = 0 ; j <= 2; j++){
                 System.out.println("Enter grade: ");
-                grade[i][i] = input.nextInt();
+                grade[i][j] = input.nextInt();
             }
         }
         for (int i = 0; i <= 2; i++){
             for (int j = 0; j <= 2; j++){
-                System.out.println(grade[i][j] + " ");
+                System.out.print(grade[i][j]);
             }
             System.out.println();
         }
